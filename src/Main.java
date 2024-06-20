fdba1516b1953f854459c8ddc93fce651db7e36e
Mireya Garcia <mireya.garcia@epn.edu.ec>
2024-06-20 09:59:17 -0500

    version 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,21 @@ import javax.swing.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-    }
+    String primernumero = JOptionPane.showInputDialog("Ingrese el primer numero entero");
+    String segundo = JOptionPane.showInputDialog("Ingrese el segundo entero");
+    String tercero = JOptionPane.showInputDialog("Ingrese el segundo entero");
+    String cuarto = JOptionPane.showInputDialog("Ingrese el tercero entero");
+    int numero1 = Integer.parseInt(primernumero);
+    int numero2 = Integer.parseInt(segundo);
+    int suma = numero1 + numero2;
+    JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma, "sUMA DE DOS ENTEROS", JOptionPane.INFORMATION_MESSAGE);
+    int resta = numero1 -  numero2;
+    JOptionPane.showMessageDialog(null, "El resultado de la resta es: "+resta, "sRESTA DE DOS ENTEROS", JOptionPane.INFORMATION_MESSAGE);
+    int multiplicacion = numero1 * numero2;
+    JOptionPane.showMessageDialog(null,"El resultado de la multiplicación es: "+multiplicacion, "sMULTI DE DOS ENTEROS", JOptionPane.INFORMATION_MESSAGE);
+    int division = numero1 / numero2;
+    JOptionPane.showMessageDialog(null, "El resultado de la division es: "+division, "sDIV DE DOS ENTEROS", JOptionPane.INFORMATION_MESSAGE);
+}
 }
--- src/form1.java
+++ src/form1.java
@@ -1,7 +1,18 @@
 import javax.swing.*;
 
 public class form1 {
-    private JButton button1;
     private JTextArea textArea1;
-    private JTextArea textArea2;
+    private JButton button1;
+    private JList list1;
+    private JRadioButton mireyaPrueba1RadioButton;
+    private JRadioButton mireyaPruba2RadioButton;
+    private JEditorPane editorPane1;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JCheckBox holACheckBox;
+    private JCheckBox hola2CheckBox;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
